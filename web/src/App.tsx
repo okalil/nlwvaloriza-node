e7331f839cdb5914ebf49e8cc5c6a881fbe3b388
Andrew Kalil Barrense Magalhães <kalilmagal@gmail.com>
2021-07-29 18:22:11 -0300

    add routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,34 @@
-import { ThemeProvider } from "styled-components";
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Route,
+    Switch,
+} from "react-router-dom";
+
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 
+import { ThemeProvider } from "styled-components";
 import light from "./styles/light";
 import GlobalStyle from "./styles/global";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
+    const loggedIn = localStorage.getItem("userToken");
+
     return (
         <Router>
             <ThemeProvider theme={light}>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/">
-                    <div>Home</div>
-                </Route>
+                <AuthContextProvider>
+                    <Switch>
+                        <Route path="/login">
+                            <Login />
+                        </Route>
+                        <Route path="/">
+                            {loggedIn ? <Home /> : <Redirect to="/login" />}
+                        </Route>
+                    </Switch>
+                </AuthContextProvider>
             </ThemeProvider>
             <GlobalStyle />
         </Router>
