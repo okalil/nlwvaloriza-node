f8dabdb842d12705600f8b25bc21d53818d21007
Andrew Kalil Barrense Magalhães <kalilmagal@gmail.com>
2021-08-02 13:44:04 -0300

    update login service

--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -1,40 +1,39 @@
-import { getCustomRepository } from "typeorm";
-import { UsersRepositories } from "../repositories/UsersRepositories";
-import { compare } from "bcryptjs";
-import { sign } from "jsonwebtoken";
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from '../repositories/UsersRepositories';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 interface IAuthenticateRequest {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 class AuthenticateUserService {
-    async execute({ email, password }: IAuthenticateRequest) {
-        const usersRepositories = getCustomRepository(UsersRepositories);
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
 
-        const user = await usersRepositories.findOne({ email });
+    const user = await usersRepositories.findOne({ email });
 
-        if (!user) {
-            throw new Error("Email/Password Incorrect");
-        }
-
-        const passwordMatch = await compare(password, user.password);
-
-        if (!passwordMatch) {
-            throw new Error("Email/Password Incorrect");
-        }
-
-        const token = sign(
-            { email: user.email },
-            "e824010cf4bd347616119e90f229fcb4",
-            {
-                subject: user.id,
-                expiresIn: "1d",
-            }
-        );
-
-        return token;
+    if (!user) {
+      throw new Error('Email/Password Incorrect');
     }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email/Password Incorrect');
+    }
+
+    const token = sign(
+      { email: user.email },
+      'e824010cf4bd347616119e90f229fcb4',
+      {
+        subject: user.id,
+        expiresIn: '1d',
+      }
+    );
+    return { token, user: { id: user.id, name: user.name, admin: user.admin } };
+  }
 }
 
 export { AuthenticateUserService };
