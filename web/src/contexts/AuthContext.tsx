62e3e6e3cc7bbf1cc0724850c20034a77694da0d
Andrew Kalil Barrense Magalhães <kalilmagal@gmail.com>
2021-08-02 17:10:48 -0300

    update auth

--- web/src/contexts/AuthContext.tsx
+++ web/src/contexts/AuthContext.tsx
@@ -1,35 +1,51 @@
-import { Dispatch, SetStateAction, useEffect } from "react";
-import { useState } from "react";
-import { createContext, ReactNode } from "react";
+import { Dispatch, SetStateAction, useState } from 'react';
+import { createContext, ReactNode } from 'react';
 
 type AuthContextType = {
-    userToken: string | null;
-    setUserToken: Dispatch<SetStateAction<string | null>>;
-    authentication: { Authorization: string };
+  userToken: string | null;
+  setUserToken: Dispatch<SetStateAction<string | null>>;
+  currentUserId: string | null;
+  setCurrentUserId: Dispatch<SetStateAction<string | null>>;
+  authentication: { Authorization: string };
 };
 
 type AuthContextProviderProps = {
-    children: ReactNode;
+  children: ReactNode;
 };
 
 export const AuthContext = createContext({} as AuthContextType);
 
 export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
-    const initialState = localStorage.getItem("userToken");
-    const [userToken, setUserToken] = useState(initialState);
-    const authentication = { Authorization: `Bearer ${userToken}` };
+  const localUser = localStorage.getItem('currentUser');
 
-    useEffect(() => {
-        if (userToken) {
-            localStorage.setItem("userToken", userToken);
-        }
-    }, [userToken]);
+  const [userToken, setUserToken] = useState(
+    localUser ? JSON.parse(localUser).token : null
+  );
+  const [currentUserId, setCurrentUserId] = useState(
+    localUser ? JSON.parse(localUser).id : null
+  );
 
-    return (
-        <AuthContext.Provider
-            value={{ userToken, setUserToken, authentication }}
-        >
-            {children}
-        </AuthContext.Provider>
-    );
+  const authentication = { Authorization: `Bearer ${userToken}` };
+
+  const createdAt = localUser ? JSON.parse(localUser).created_at : undefined;
+  const expired = Date.now() - createdAt > 24 * 60 * 60 * 1000;
+
+  if (createdAt && expired) {
+    console.log((Date.now() - createdAt) / 1000);
+    console.log(expired);
+    localStorage.removeItem('currentUser');
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        userToken,
+        setUserToken,
+        currentUserId,
+        setCurrentUserId,
+        authentication,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,124 +1,77 @@
-import React, { FormEvent } from "react";
-import { useHistory } from "react-router-dom";
+import React, { FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
+import { api } from '../services/api';
+import { useAuth } from '../hooks/useAuth';
 
-import { Box } from "../components/Box";
-import { Button } from "../components/Button";
-
-// import {} from './styles'
-import styled from "styled-components";
-import { api } from "../services/api";
-
-const Wrapper = styled.main`
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
-
-const Container = styled.div`
-    width: min(85%, 1100px);
-
-    display: flex;
-    gap: 1rem;
-    flex-direction: column;
-
-    @media (min-width: 768px) {
-        flex-direction: row;
-        justify-content: space-between;
-    }
-
-    hr {
-        color: ${({ theme }) => theme.colors.hr};
-        margin: 0.5rem 0;
-    }
-`;
-
-const Title = styled.h1`
-    color: #9c64e2;
-`;
-
-const About = styled.h2`
-    font-size: 1.25rem;
-    font-weight: 500;
-`;
-
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    gap: 1rem;
-
-    button {
-        background: ${({ theme }) => theme.colors.bgBrand};
-    }
-`;
-
-const FormControl = styled.input`
-    border: 1px solid #ddd;
-    border-radius: ${({ theme }) => theme.borderRadius};
-    padding: 0.25rem 0.5rem;
-
-    &::placeholder {
-        color: ${({ theme }) => theme.colors.placeholder};
-    }
-`;
-
-const GreenButton = styled(Button)`
-    background: #04d361;
-    display: block;
-    margin: 0 auto;
-`;
+import { Box } from '../components/Box';
+import { Button } from '../components/Button';
+import {
+  Wrapper,
+  Container,
+  Title,
+  About,
+  Form,
+  FormControl,
+  GreenButton,
+} from '../styles/login';
 
 const Login: React.FC = () => {
-    document.title = "Login | Valoriza";
-    const history = useHistory();
+  document.title = 'Login | Valoriza';
+  const history = useHistory();
+  const { setUserToken, setCurrentUserId } = useAuth();
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
 
-    async function handleSubmit(event: FormEvent) {
-        event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
 
-        const formData = new FormData(event.target as HTMLFormElement);
+    try {
+      const { data } = await api.post('/login', {
+        headers: { 'Content-Type': 'application/json' },
+        email: formData.get('email'),
+        password: formData.get('password'),
+      });
+      const { token, user } = data;
+      setUserToken(token);
+      setCurrentUserId(user.id);
+      localStorage.setItem(
+        'currentUser',
+        JSON.stringify({
+          token,
+          id: user.id,
+          created_at: Date.now(),
+        })
+      );
 
-        try {
-            const response = await api.post("/login", {
-                headers: { "Content-Type": "application/json" },
-                email: formData.get("email"),
-                password: formData.get("password"),
-            });
-            localStorage.setItem("userToken", response.data);
-
-            history.push("/");
-        } catch (error) {
-            if (error.response) {
-                console.log(error.response.data);
-            } else if (error.request) {
-            } else {
-                console.log("Error", error.message);
-            }
-        }
+      history.push('/');
+    } catch (error) {
+      if (error.message) {
+        console.log(error.message);
+      }
     }
+  }
 
-    return (
-        <Wrapper>
-            <Container>
-                <div>
-                    <Title>Valoriza</Title>
-                    <About>
-                        O Valoriza promove o reconhecimento entre os
-                        companheiros de equipe.
-                    </About>
-                </div>
+  return (
+    <Wrapper>
+      <Container>
+        <div>
+          <Title>Valoriza</Title>
+          <About>
+            O Valoriza promove o reconhecimento entre os companheiros de equipe.
+          </About>
+        </div>
 
-                <Box>
-                    <Form onSubmit={handleSubmit}>
-                        <FormControl name="email" placeholder="Email" />
-                        <FormControl name="password" placeholder="Senha" />
-                        <Button>Entrar</Button>
-                    </Form>
-                    <hr />
-                    <GreenButton>Criar conta</GreenButton>
-                </Box>
-            </Container>
-        </Wrapper>
-    );
+        <Box>
+          <Form onSubmit={handleSubmit}>
+            <FormControl name="email" placeholder="Email" />
+            <FormControl name="password" placeholder="Senha" />
+            <Button>Entrar</Button>
+          </Form>
+          <hr />
+          <GreenButton>Criar conta</GreenButton>
+        </Box>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Login;
